41546ea5adf4d3876f7d14ee98b6bdd39193f186
Allana-Morris <ST10204772@vcconnect.edu.za>
2024-10-06 22:45:21 +0200

    Registerstration Frontend

--- frontend/src/components/CustomerRegistration/CustomerRegistration.mjs
+++ frontend/src/components/CustomerRegistration/CustomerRegistration.mjs
@@ -1,17 +1,18 @@
 // src/components/CustomerRegistration/CustomerRegistration.js
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import RegistrationForm from './RegistrationForm';
-import { styles } from './styles';
+import { styles } from './RegisterStyles.css';
 
 function CustomerRegistration() {
   return (
     <div style={styles.container}>
       <h2 style={styles.header}>Customer Registration Form</h2>
       <RegistrationForm />
-      <div style={styles.loginLinkContainer}>
-        <a href="/login" style={styles.loginLink}>An existing customer? Click to Login</a>
-      </div>
+      <Link to="/login" style={styles.loginLink}>
+          An existing customer? Click to Login
+        </Link>
     </div>
   );
 }
