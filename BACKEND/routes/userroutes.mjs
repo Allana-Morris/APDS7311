6d4dee70d85dfc173bebe05ff74a26751968f5ed
Allana-Morris <ST10204772@vcconnect.edu.za>
2024-10-08 19:55:29 +0200

    hhh

--- BACKEND/routes/userroutes.mjs
+++ BACKEND/routes/userroutes.mjs
@@ -128,24 +128,24 @@ router.post('/', async (req, res) => {
 
             if (!user)
             {
-                return res.status(401).json({message: "Auth failed"});
+               // return res.status(401).json({message: "Auth failed"});
             }
 
             const passwordMatch = await bcrypt.compare(password, user.password)
 
             if (!passwordMatch)
             {
-                return res.status(401).json({message:"auth failed"})
+                //return res.status(401).json({message:"auth failed"})
             }
             else{
                 const token = jwt.sign({accountNumber: accountNumber}, jwtSecret, {expiresIn:"1h"})
-                res.status(200).json({message: "authentication succ", token: token, name: req.body.name});
+              //  res.status(200).json({message: "authentication succ", token: token, name: req.body.name});
             }
         }
         catch (error)
         {
             console.error("Login error:", error)
-            res.status(500).json({message: "Login"} )
+           // res.status(500).json({message: "Login"} )
         }
     });
 
