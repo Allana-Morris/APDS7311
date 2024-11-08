d9ba4250eb961086b0c1da965b686b420afddf6f
Allana-Morris <ST10204772@vcconnect.edu.za>
2024-11-08 14:11:13 +0200

    Hello

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import Log from "./CustomerLogin/LoginForm.mjs"
 import Dash from "./CustomerDashboard/DashboardForm.mjs"
 import InternationalPayment from "./RecipientDetailsPay/DetailsForm.mjs"
 import LocalPayment from "./RecipientDetailsPay/localPaymentForm.mjs"
+//part 3
+import EmpLog from "./EmployeeLogin/EmpLoginForm.mjs"
+import EmpDash from "./EmployeeDashBoard/EmpDashboardForm.mjs"
+import Verify from "./EmployeeVerify/paymentVerifyForm.mjs"
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";  // Import BrowserRouter and Route
 
@@ -12,11 +16,21 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Reg />} />
+
+          <Route exact path="/Employee" element={<EmpLog />} />
+          <Route exact path="/Registration" element={<Reg />} />
+
           <Route exact path="/Login" element={<Log />} />
-          <Route exact path = "/Home" element={< Dash/>}/>
-          <Route exact path = "/LocalPayment" element={< LocalPayment/>}/>
-          <Route exact path = "/InterPayment" element={< InternationalPayment/>}/>
+          <Route exact path="/Home" element={< Dash />} />
+          <Route exact path="/LocalPayment" element={< LocalPayment />} />
+          <Route exact path="/InterPayment" element={< InternationalPayment />} />
+
+
+          <Route exact path="/EmployeeHome" element={< EmpDash />} />
+          <Route exact path="/verifyPay" element={< Verify />} />
+
+
+
 
         </Routes>
       </BrowserRouter>
