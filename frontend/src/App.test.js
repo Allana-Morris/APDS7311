dac37112284f31ff413a0c9b9934861ca0947a64
Allana-Morris <ST10204772@vcconnect.edu.za>
2024-11-08 14:33:32 +0200

    cutie patootie

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,8 +4,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   act(() => {
-  render(<App />);
+    render(<App />);
   });
-  const linkElement = screen.getByText();
+  // Replace 'Learn React' with the actual text you expect to render in App
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
