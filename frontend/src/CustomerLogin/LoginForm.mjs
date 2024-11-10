779ef09201279875758d1ab2c7f06b9fa8bb9908
Allana-Morris <ST10204772@vcconnect.edu.za>
2024-11-10 15:13:37 +0200

    hello

--- frontend/src/CustomerLogin/LoginForm.mjs
+++ frontend/src/CustomerLogin/LoginForm.mjs
@@ -61,7 +61,7 @@ function LoginForm() {
           id="account-number"
           value={accountNumber}
           onChange={(e) => setAccountNumber(e.target.value)}
-          placeholder="Account Number"
+          placeholder="Account Number or Username"
           required
         />
 
